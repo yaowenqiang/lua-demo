72a16aac8d14df419f0c4ec82e4f2822ef85dbcd
yaowenqiang <yaowenqiang111@163.com>
2014-05-02 23:08:40 +0800

    get input from user

--- guessmynumber.lua
+++ guessmynumber.lua
@@ -2,5 +2,6 @@ print "Guess a number"
 math.randomseed(os.time())
 math.random()
 number = math.random(100)
-print(number)
-
+--print(number)
+answer = io.read('*n') --Read input from user as a number
+print(answer)
