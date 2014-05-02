1d6429c0aa1479fd859e3d02a4066ffdf54cc991
yaowenqiang <yaowenqiang111@163.com>
2014-05-02 23:52:13 +0800

    add the while loop

--- guessmynumber.lua
+++ guessmynumber.lua
@@ -3,12 +3,14 @@ math.randomseed(os.time())
 math.random()
 number = math.random(100)
 --print(number)
-answer = io.read('*n') --Read input from user as a number
---print(answer)
-if answer < number then
-    print "Too Low"
-elseif answer > number then
-    print "Too High"
-else 
-    print "You got it"
+while answer ~= number do
+    answer = io.read('*n') --Read input from user as a number
+    --print(answer)
+    if answer < number then
+        print "Too Low"
+    elseif answer > number then
+        print "Too High"
+    else 
+        print "You got it"
+    end
 end
