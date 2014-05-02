f5127457e0bf9861f87775c8af81059dc255c8f7
yaowenqiang <yaowenqiang111@163.com>
2014-05-03 04:58:51 +0800

    add the table array demo code

--- guessmynumber.lua
+++ guessmynumber.lua
@@ -7,8 +7,10 @@ function handleGuesses(lowMessage,highMessage,number)
 --function handleGuesses(...)
     --lowMessage,highMessage,number = ...
     --lowMessage,highMessage,number = ...
+    local guesses = {}
     while answer ~= number do
         local answer = io.read('*n') --Read input from user as a number
+        guesses[#guesses+1] = answer
         --print(answer)
         if answer < number then
             print(lowMessage)
@@ -19,11 +21,16 @@ function handleGuesses(lowMessage,highMessage,number)
         end
         print "Guess again"
     end
-    
+return guesses    
 end
 local number = getRandomNumber(100)
 print "Guess a number"
-handleGuesses("Too low","Too high",number)
+local guesses = handleGuesses("Too low","Too high",number)
 --print(number)
 print "You got it"
 --print(answer)
+
+print("Here are your guesses")
+for k, v in ipairs(guesses) do
+    print(k,v)
+end
