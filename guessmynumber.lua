5d592085cbee95a97972b24ed7ee00ee134a789b
yaowenqiang <yaowenqiang111@163.com>
2014-05-03 00:30:04 +0800

    add the handleGuesses Variadic function

--- guessmynumber.lua
+++ guessmynumber.lua
@@ -3,20 +3,25 @@ function getRandomNumber(maxValue)
     math.random()
     return math.random(maxValue)
 end
+function handleGuesses(...)
+    lowMessage,highMessage,number = ...
+    while answer ~= number do
+        local answer = io.read('*n') --Read input from user as a number
+        --print(answer)
+        if answer < number then
+            print(lowMessage)
+        elseif answer > number then
+            print(highMessage)
+        else 
+            break
+        end
+        print "Guess again"
+    end
+    
+end
 local number = getRandomNumber(100)
 print "Guess a number"
+handleGuesses("Too low","Too high",number)
 --print(number)
-while answer ~= number do
-    local answer = io.read('*n') --Read input from user as a number
-    --print(answer)
-    if answer < number then
-        print "Too Low"
-    elseif answer > number then
-        print "Too High"
-    else 
-        break
-    end
-    print "Guess again"
-end
 print "You got it"
 --print(answer)
