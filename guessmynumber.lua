99bcc375eb1c145f2085a3fac16cffd8cf0d7acb
yaowenqiang <yaowenqiang111@163.com>
2014-05-03 00:18:51 +0800

    add the getRandomNumber function

--- guessmynumber.lua
+++ guessmynumber.lua
@@ -1,7 +1,10 @@
+function getRandomNumber(maxValue)
+    math.randomseed(os.time())
+    math.random()
+    return math.random(maxValue)
+end
+local number = getRandomNumber(100)
 print "Guess a number"
-math.randomseed(os.time())
-math.random()
-local number = math.random(100)
 --print(number)
 while answer ~= number do
     local answer = io.read('*n') --Read input from user as a number
@@ -16,4 +19,4 @@ while answer ~= number do
     print "Guess again"
 end
 print "You got it"
-print(answer)
+--print(answer)
