4fd81dcc1b4ca3d0e5b4f37db364180b8c9ab5cd
yaowenqiang <yaowenqiang111@163.com>
2014-05-02 22:24:06 +0800

    first init

---
+++ guessmynumber.lua
@@ -0,0 +1,6 @@
+print "Guess a number"
+math.randomseed(os.time())
+math.random()
+number = math.random(100)
+print(number)
+
---
+++ variable.lua
@@ -0,0 +1,6 @@
+luay = "ConMan"
+x = 1
+y = 2
+x,y = `0,20`
+x,y = y,x
+print(x);
