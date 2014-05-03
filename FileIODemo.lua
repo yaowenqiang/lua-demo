738ac709655aa4ac8d9a26b6efd605732875bcb0
yaowenqiang <yaowenqiang111@163.com>
2014-05-03 22:35:45 +0800

    add the read file demo

--- FileIODemo.lua
+++ FileIODemo.lua
@@ -1,4 +1,15 @@
-local file = io.open("test.text",'w')
-file:write("Testing 1,2,3\n")
-file:write("abc 123\n")
-file:close()
+--local file = io.open("test.text",'w')
+--file:write("Testing 1,2,3\n")
+--file:write("abc 123\n")
+--file:close()
+
+local file = io.open("test.text","r")
+--text = file:read("*a")
+    --*a = reads while file
+    --*l = reads netx line
+    --*n = reads a number
+    --x  = read x characters
+--print(text)
+for line in file:lines() do
+    print(line)
+end
