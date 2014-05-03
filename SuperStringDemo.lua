e2a27b2ef86588a36a7779f00cbad25d18910586
yaowenqiang <yaowenqiang111@163.com>
2014-05-03 12:23:49 +0800

    add the rever function

--- SuperStringDemo.lua
+++ SuperStringDemo.lua
@@ -9,9 +9,16 @@ end
 function ss.add(s1,s2)
     return s1.s .. s2.s
 end
+
 meta.__add = ss.add
+meta.__index = function(table,key)
+    if key == "reverse" then
+        return string.reverse(table.s)
+    end
+end
 
 firstname = ss.new("Bill")
 lastname = ss.new("Gates")
 print(firstname + lastname)
+print(firstname.reverse )
 --print(firstname.s)
