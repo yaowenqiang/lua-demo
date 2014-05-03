c6c4d193d0ad7c32cfb17a63d399a33f78593b32
yaowenqiang <yaowenqiang111@163.com>
2014-05-03 21:37:15 +0800

    add the string find dem code

---
+++ StringLibraryDemo.lua
@@ -0,0 +1,284 @@
+text = [[
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css" media="all">
+        .gray{
+            background-color:#ccc;
+            padding:20px;
+        }
+        .col-md-9 {
+            background-color:#999;
+        }
+    </style>
+  </head>
+  <body>
+      <div class="gray">
+          <div class="container">
+              <div class="well">
+                  Container
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="well">Container</div>
+              </div>
+              <div class="row">
+                  <div class="well">Container</div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-6">
+                    <div class="well">Column 1</div>
+                  </div>
+                  <div class="col-lg-6">
+                    <div class="well">Column 2</div>
+                  </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-4">
+                    <div class="well">Column 1</div>
+                  </div>
+                  <div class="col-lg-8">
+                    <div class="well">Column 2</div>
+                  </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-4">
+                    <div class="well">Column 1</div>
+                  </div>
+                  <div class="col-lg-4">
+                    <div class="well">Column 2</div>
+                  </div>
+                  <div class="col-lg-2">
+                    <div class="well">Column 3</div>
+                  </div>
+                  <div class="col-lg-2">
+                    <div class="well">Column 4</div>
+                  </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                <div class="col-lg-6">
+                    <div class="well">column 1</div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="well">column 2</div>
+                </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                <div class="col-md-3">
+                    <div class="well">column 1</div>
+                </div>
+                <div class="col-md-3">
+                    <div class="well">column 2</div>
+                </div>
+                <div class="col-md-3">
+                    <div class="well">column 3</div>
+                </div>
+                <div class="col-md-3">
+                    <div class="well">column 4</div>
+                </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                <div class="col-sm-3">
+                    <div class="well">column 1</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="well">column 2</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="well">column 3</div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="well">column 4</div>
+                </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                <div class="col-xs-3">
+                    <div class="well">column 1</div>
+                </div>
+                <div class="col-xs-3">
+                    <div class="well">column 2</div>
+                </div>
+                <div class="col-xs-3">
+                    <div class="well">column 3</div>
+                </div>
+                <div class="col-xs-3">
+                    <div class="well">column 4</div>
+                </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="col-xs-6 col-md-3">
+                      <div class="well">
+                            column 1
+                      </div>
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                      <div class="well">
+                            column 2
+                      </div>
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                      <div class="well">
+                            column 3
+                      </div>
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                      <div class="well">
+                            column 4
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="col-xs-6 col-md-3">
+                      <div class="well">
+                            column 1
+                            column 1
+                            column 1
+                            column 1
+                            column 1
+                      </div>
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                      <div class="well">
+                            column 2
+                      </div>
+                  </div>
+                  <div class="clearfix hidden-md hidden-lg"></div>
+                  <div class="col-xs-6 col-md-3">
+                      <div class="well">
+                            column 3
+                      </div>
+                  </div>
+                  <div class="col-xs-6 col-md-3">
+                      <div class="well">
+                            column 4
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                <div class="col-md-4">
+                    <div class="well">Column 1</div>
+                </div>
+                <div class="col-md-4 col-md-offset-4">
+                    <div class="well">Column 2</div>
+                </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                <div class="col-md-4">
+                    <div class="well">Column 1</div>
+                </div>
+                <div class="col-md-4 col-md-offset-2">
+                    <div class="well">Column 2</div>
+                </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                <div class="col-sm-4">
+                    <div class="well">Column 1</div>
+                </div>
+                <div class="col-sm-4 col-sm-offset-4">
+                    <div class="well">Column 2</div>
+                </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-9">column 1
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="well">Nested Column 1</div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="well">Nested Column 2</div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col-md-3">column 2</div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-6 col-md-push-6">
+                      <div class="well">Column 1</div>
+                   </div>
+                   <div class="col-md-6  col-md-pull-6">
+                      <div class="well">Column 2</div>
+                   </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                    <div class="col-sm-3">
+                        <div class="well">
+                            <img class='img-responsive hidden-sm'  src="images/client-logo1.png" alt="">
+                        </div>
+                    </div>
+              </div>
+              <div class="row">
+                    <div class="col-sm-3">
+                        <div class="well">
+                            <img class='img-responsive hidden-xs'  src="images/client-logo1.png" alt="">
+                        </div>
+                    </div>
+              </div>
+              <div class="row">
+                    <div class="col-sm-3">
+                        <div class="well">
+                            <img class='img-responsive visiable-sm'  src="images/client-logo1.png" alt="">
+                        </div>
+                    </div>
+              </div>
+          </div>
+      </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+]]
+--b,e = text:find("stringToFind")
+--string.find(text,"stringToFind")
+--b,e = text:find("div")
+--print(b,e)
+--print(string.sub(text,b,e))
+b,e = text:find("<.->")
+print(string.sub(text,b,e))
+
