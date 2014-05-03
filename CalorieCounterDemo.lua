075f3d5423b86c6259ee330fa473cef33a32e213
yaowenqiang <yaowenqiang111@163.com>
2014-05-03 12:45:12 +0800

    add class demo

--- CalorieCounterDemo.lua
+++ CalorieCounterDemo.lua
@@ -2,12 +2,20 @@ CalorieCounter = {count = 0,goal = 2000}
 function CalorieCounter:add(amount)
     self.count = self.count + amount
 end
-function CalorieCounter.didReachGoal()
-    return self.count == self.goal
+function CalorieCounter:didReachGoal()
+    return self.count >= self.goal
 end
-function CalorieCounter.new()
+function CalorieCounter:new(t)
     t = t or {}
     setmetatable(t,self)
     self.__index = self
     return t
 end
+c = CalorieCounter:new{goal = 1500}
+print(c.goal)
+c:add(500)
+print(c.count)
+print(c:didReachGoal())
+c:add(1000)
+print(c.count)
+print(c:didReachGoal())
